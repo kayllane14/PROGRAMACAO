e5a01fa5bc2f5bc686e79cc07b5be80c7e11d678
hercilio17t <hercilio.tenorio@ufrpe.br>
2023-06-26 21:44:49 -0300

    CLASSES E ALTERAÇÕES

--- Miniprojeto1/src/miniprojetohercilio.java
+++ Miniprojeto1/src/miniprojetohercilio.java
@@ -3,128 +3,101 @@ import java.util.Scanner;
 
 public class miniprojetohercilio {
         public static void main(String[] args) throws Exception {
-                System.out.println("Olá, seja bem vindo ao Dungeons! " +
+
+                // MENSAGEM DE BOAS VINDAS.
+                System.out.println("Olá, seja bem vindo a Nômades! " +
                                 "Para iniciarmos nossa aventura, escolha o seu nome de usuário:");
                 Scanner escaneador = new Scanner(System.in);
                 String usuario = escaneador.nextLine();
 
-                String escolhas = "Bem-vindo, " + usuario + "! Escolha, através da sua numeração, sua vocação:";
+                // CRIAÇÃO DO PRIMEIRO PERSONAGEM (usuário) ATRAVÉS DA NOSSA CLASSE CRIADA.
+                PersonagemHumano nome = new PersonagemHumano(usuario, 100);
 
-                escolhas += System.lineSeparator()
-                                + "1. Knight: Mestre de combate corpo a corpo, possuindo resistência imensa.";
-                escolhas += System.lineSeparator()
-                                + "2. Paladin: Mestre de luta à distância, domínio leve da artes mágicas.";
+                System.out.println("Olá, " + usuario
+                                + ". Seja BEM-VINDO! Que tal um parceiro? Vamos dar um nome ao seu companheiro:");
+                String companheiro = escaneador.nextLine();
 
-                System.out.println(escolhas);
-                int escolha = escaneador.nextInt();
+                // CRIAÇÃO DO SEGUNDO PERSONAGEM (lobo) ATRAVÉS DA NOSSA CLASSE.
+                PersonagemAnimal dog = new PersonagemAnimal(companheiro, 100);
 
-                switch (escolha) {
+                // INICIO DA HISTÓRIA.
+                System.out.println("Agora que podemos nos comunicar com você, vamos iniciar!");
+                System.out.println();
+                System.out.println(usuario + " decidiu se tornar nômade após ver que estava cansado da rotina" +
+                                " de trabalho cansativa e repetitiva. Ele decidiu que iria viajar pelo mundo e que, estava pronto para novas aventuras!");
+                System.out.println("Junto a ele, obvio que não iria faltar o seu fiel e leal companheiro: "
+                                + companheiro +
+                                ", que estará com ele nessa jornada onde visitará vários países em seu MotorHome: o Mate!");
 
-                        case 1:
-                                System.out.println("Parabéns, você se tornou um Knight! Suas características são:" +
-                                                " Rápidos em avanço de fist, sword, club e axe fighting, shielding. Avanços muito lentos na magia.");
-                                break;
+                // CAPITULO 1 -> PARIS
+                System.out.println("Ao trancos e barrancos, " + usuario
+                                + " conseguiu chegar a Paris, seu primeiro destino. Lá, ele vê " +
+                                "que seus mantimentos estão acabando, e precisa ir no mercado. No caminho, nota que uma luz estranha acende no painel "
+                                +
+                                "do Mate.");
+                System.out.println("Vamos a sua primeira decisão!");
+                String decisao1 = "1. Parar na oficina mais próxima e resolver o problema do Mate";
+                decisao1 += System.lineSeparator() + "2. Deixar para resolver depois e seguir até o mercado";
 
-                        case 2:
-                                System.out.println("Parabéns, você se tornou um Paladin! Suas características são:" +
-                                                " Rápidos em avanço de distance fighting, razoável com qualquer outra habilidade.");
-                                break;
+                System.out.println(decisao1);
 
-                        default:
-                                System.out.println("Essa opção não existe, tente novamente!");
-                }
+                int escolha1 = escaneador.nextInt();
 
-                System.out.println("Agora que você já sabe qual sua vocação, vamos iniciar nossa jornada." +
-                                " Digite 1 para entrar na Dungeon!");
-                int entrada = escaneador.nextInt();
-
-                String inicio = "Ao entrar na Dungeon, você deu de cara com um Ghost." +
-                                " O que deseja fazer?";
-                inicio += System.lineSeparator() + "1. Habilidade básica";
-                inicio += System.lineSeparator() + "2. Habilidade especial";
-
-                System.out.println(inicio);
-
-                int fase1 = escaneador.nextInt();
-                if (fase1 == 1) {
-                        System.out.println(
-                                        "Mesmo sofrendo um pouco, você conseguiu vencer o Ghost! Pode seguir sua jornada.");
-                } else if (fase1 == 2) {
-                        System.out.println("Você aniquilou o Ghost!! Pode seguir sua jornada.");
+                if (escolha1 == 1) {
+                        System.out.println(usuario + " conseguiu chegar a oficina antes que o problema se agravasse." +
+                                        " Em poucas horas já havia consertado o Mate e ido até o mercado repor os mantimentos. Agora só curtir a cidade Luz.");
                 } else {
-                        System.out.println("Você não possui essa habilidade.");
+                        System.out.println(
+                                        "Mate acabou não aguentando o caminho até o mercado e parou em um bairro perigoso. "
+                                                        + usuario +
+                                                        " notou alguém se aproximava e decidiu pedir ajuda.");
+                        System.out.println("Infelizmente, as intenções daquele que se aproximava não eram boas." +
+                                        " O rapaz veio até a direção do " + usuario
+                                        + " com uma faca na mão e, ao notar o comportamento, " + companheiro +
+                                        " saltou em cima do possível ladrão. Ele conseguiu salvar os dois mas acabou saindo ferido...");
+                        dog.saudeanimal(-30);
                 }
 
-                System.out.println("Parabéns por passar da primeira fase! Digite 1 para" +
-                                " seguirmos para o próximo nível.");
-                int entrada2 = escaneador.nextInt();
+                System.out.println("Obvio que apesar dos desafios, " + usuario + " e " + companheiro
+                                + " não iriam deixar de visitar a famosa" +
+                                " Torre Eiffel.");
+                System.out.println();
+                System.out.println(
+                                "Chegando lá, notam uma multidão indo até uma barraca e, curiosos, vão até lá. Acontece que "
+                                                +
+                                                "uma grande emissora francesa, estava fazendo um jogo de perguntas e respostas e quem acertasse mais, levaria um "
+                                                +
+                                                "prêmio de $20.000,00 para casa!");
 
-                String meio = "Enquanto explorava o novo andar da Dungeon, você foi atacado por um" +
-                                " Carniphilas enquanto estava desatento. Por estar ferido, você está limitado nessa batalha, o que irá fazer?";
-                meio += System.lineSeparator() + "1. Tentar fugir do Carniphilas.";
-                meio += System.lineSeparator() + "2. Usar sua habilidade especial para e evaporá-lo.";
+                String decisao2 = "1. Participar e utilizar seus conhecimentos que buscou antes de virar nômade.";
+                decisao2 += System.lineSeparator()
+                                + "2. Achar que é perda de tempo e ir aproveitar a bela vista da Torre para algumas fotos.";
 
-                System.out.println(meio);
+                System.out.println(decisao2);
 
-                int fase2 = escaneador.nextInt();
-                if (fase2 == 1) {
-                        System.out.println("Você conseguiu fugir mas esta muito debilitado, todo cuidado será pouco" +
-                                        " a partir de agora. Digite 1 para continuar:");
-                        int seguir = escaneador.nextInt();
+                int escolha2 = escaneador.nextInt();
 
-                        String potion = "Você está chegando a fase final da Dugeon quando encontra uma feiticeira." +
-                                        " Ela te oferece uma poção e promete que irá te curar.";
-                        potion += System.lineSeparator() + "1. Aceitar";
-                        potion += System.lineSeparator() + "2. Recusar";
-
-                        System.out.println(potion);
-
-                        int escolha2 = escaneador.nextInt();
-                        if (escolha2 == 1) {
-                                System.out.println("Você acreditou na feiticeira e foi envenenado." +
-                                                " Sua ingenuidade acabou com sua passagem por Dundgeons.");
-                                System.out.println("Você fracassou!");
-                        } else if (escolha2 == 2) {
-                                System.out.println("Graças a essa decisão, você seguirá vivo e ao passar pela escada" +
-                                                "da ultima fase da Dungeon, será curado.");
-                                System.out.println("Digite 1 para subir a escada e seguir sua jornada:");
-                                int subirescada = escaneador.nextInt();
-
-                                System.out.println(
-                                                "Você está quase lá, agora só falta derrotar o poderoso Spectral. Digite 1 quando estiver pronto!");
-                                int chefe = escaneador.nextInt();
-
-                                String chefao = "O Spectral te ataca e você consegue desviar, o que fará agora?";
-                                chefao += System.lineSeparator() + "1. Usar sua habilidade especial e tentar acabar" +
-                                                " com a lutar de uma vez por todas.";
-                                chefao += System.lineSeparator()
-                                                + "2. Esperar que ele ataque novamente para ficar mais exposto";
-
-                                System.out.println(chefao);
-
-                                int escolhafinal = escaneador.nextInt();
-
-                                if (escolhafinal == 1) {
-                                        System.out.println(
-                                                        "Sua habilidade acertou em cheio o Spectral. Parabéns, você concluiu a Dungeon"
-                                                                        +
-                                                                        " e entrará na lista dos poucos nomes que conseguiram finalizar Dundgeons.");
-                                        System.out.println("Obrigado por ler Dudgeons!");
-                                } else {
-                                        System.out.println("Você não conseguiu desviar do segundo ataque do Spectral" +
-                                                        ", o ataque acertou você em um ponto vital e você acabou morrendo."
-                                                        +
-                                                        " Sinto muito, sua jornada acaba aqui!");
-                                        System.out.println("Obrigado por ler Dudgeons!");
-                                }
-
-                        }
-
-                } else if (fase2 == 2) {
-                        System.out.println("Além de estar ferido, você acabou gastando muita energia." +
-                                        " Agora, você está totalmente debilitado e não irá conseguir seguir sua jornada.");
-                        System.out.println("Você fracassou na 2ª Fase de Dudgeons.");
-                        System.out.println("Obrigado por ler Dudgeons!");
+                if (escolha2 == 1) {
+                        System.out.println(usuario + " sai incredulo e saltitante. COM $20.000,00 NA CONTA!.");
+                        System.out.println("Em meio a comemoração, " + usuario
+                                        + " percebe uma inquietação do seu companheiro " + companheiro +
+                                        " . Acontece que sua ferida está infeccionada.");
+                        System.out.println(
+                                        "Chegando no veterinário, descobre que o problema é maior do que imaginava..." +
+                                                        " Será necessária uma cirurgia e o valor não é um problema graças aquela premiação.");
+                        System.out.println();
+                        System.out.println("Após algum tempo de espera, " + usuario
+                                        + " já pode ir ver seu companheiro novamente." +
+                                        " Ele se emociona ao ver seu amigo bem e nem imagina o que iria fazer se o pior tivesse acontecido.");
+                        dog.saudeanimal(+30);
+                } else {
+                        System.out.println("Durante as fotos, " + usuario
+                                        + " percebe uma inquietação do seu companheiro " + companheiro +
+                                        " . Acontece que sua ferida está infeccionada.");
+                        System.out.println(
+                                        "Chegando no veterinário, descobre que o problema é maior do que imaginava..." +
+                                                        " Será necessária uma cirurgia e o valor é absurdo, infelizmente não pode pagar.");
+                        dog.saudeanimal(-100);
                 }
 
         }
