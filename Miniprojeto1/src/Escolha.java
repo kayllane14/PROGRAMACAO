2f39de7051a86c0722a8f4bdea295ed0e63ef612
kayllane14 <kayllane.lima@ufrpe.br>
2023-08-22 20:30:48 -0300

    miniprojeto-5- kayllane

---
+++ Miniprojeto1/src/Escolha.java
@@ -0,0 +1,16 @@
+public class Escolha {
+    
+    int texto;
+    Capitulo proximo;
+
+
+    Escolha(int texto, Capitulo proximo)
+    {
+        this.texto = texto;
+        this.proximo = proximo;
+        
+    }
+
+
+    
+}
