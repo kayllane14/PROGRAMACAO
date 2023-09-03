bab46a71115dabfbf783ad2f46b01a7588cad424
kayllane14 <kayllane.lima@ufrpe.br>
2023-09-02 22:23:26 -0300

    COMMIT

--- Miniprojeto1/src/MiniProjetoKayllane.java
+++ Miniprojeto1/src/MiniProjetoKayllane.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MiniProjetoKayllane {
+public class historiainterativaKayllane {
     public static void main(String[] args) {
        LeitorDeCarregamento leitor = new LeitorDeCarregamento();
 
